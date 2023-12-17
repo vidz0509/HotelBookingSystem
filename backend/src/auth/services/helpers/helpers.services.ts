1a51ef36893008966b510aaae130774b8a39435c
Yashbhimani623 <bhimaniyash623@gmail.com>
2023-12-17 13:46:46 +0530

    Password Validation

--- backend/src/auth/services/helpers/helpers.services.ts
+++ backend/src/auth/services/helpers/helpers.services.ts
@@ -19,6 +19,12 @@ export class HelpersServices {
     };
     return response;
   }
+
+  async isValidPassword(password) {
+    const regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\-]).{8,15}$/; / Min 8 chars Max 12 chars: 1 digit 1 Uppercase and 1 special char /
+    return regex.test(password);
+  }
+
   async buildAuthResponse(userData: User) {
     const userId = userData._id.toString();
     // const expiresIn = isRefreshToken ? parseInt(process.env.REFRESH_TOKEN_EXPIRATION_HOURS) * 3600 : parseInt(process.env.ACCESS_TOKEN_EXPIRATION_TIME);
