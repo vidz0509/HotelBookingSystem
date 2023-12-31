31dc70d4564790e9beb87eb9a1e4e912b36ad8c5
Kavita Bavadiya <kavitabavadiya@gmail.com>
2023-12-31 21:11:56 +0530

    Remove left columns - Location Section

--- frontend/users/src/pages/Home/sections/DesignBlocks.js
+++ frontend/users/src/pages/Home/sections/DesignBlocks.js
@@ -32,19 +32,9 @@ import ExampleCard from "pages/Presentation/components/ExampleCard";
 import data from "pages/Presentation/sections/data/designBlocksData";
 
 function DesignBlocks() {
-  const renderData = data.map(({ title, description, items }) => (
+  const renderData = data.map(({ title, items }) => (
     <Grid container spacing={3} sx={{ mb: 10 }} key={title}>
-      <Grid item xs={12} lg={3}>
-        <MKBox position="sticky" top="100px" pb={{ xs: 2, lg: 6 }}>
-          <MKTypography variant="h3" fontWeight="bold" mb={1}>
-            {title}
-          </MKTypography>
-          <MKTypography variant="body2" fontWeight="regular" color="secondary" mb={1} pr={2}>
-            {description}
-          </MKTypography>
-        </MKBox>
-      </Grid>
-      <Grid item xs={12} lg={9}>
+      <Grid item xs={12} lg={12}>
         <Grid container spacing={3}>
           {items.map(({ image, name, count, route, pro }) => (
             <Grid item xs={12} md={4} sx={{ mb: 2 }} key={name}>
