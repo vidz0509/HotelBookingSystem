5ca0732e6a1816795c66c4f48332f6cfb5c923c8
Yashbhimani623 <bhimaniyash623@gmail.com>
2024-01-03 00:01:34 +0530

    html form comment

--- frontend/users/src/pages/Home/index.js
+++ frontend/users/src/pages/Home/index.js
@@ -109,6 +109,52 @@ function Home() {
           boxShadow: ({ boxShadows: { xxl } }) => xxl,
         }}
       >
+        {/* <form action="reservation.php" method="post">
+          <div>
+            <label>Your Name</label>
+            <input type="text" id="name" name="visitor_name" placeholder="John Doe" required />
+          </div>
+          <div>
+            <label>Your E-mail</label>
+            <input type="email" id="email" name="visitor_email" placeholder="john.doe@email.com" required />
+          </div>
+          <div>
+            <label>Your Phone</label>
+            <input type="tel" id="phone" name="visitor_phone" placeholder="498-348-3872" required />
+          </div>
+          <hr />
+          <div>
+            <label>Adults</label>
+            <input type="number" id="adult" name="total_adults" placeholder="2" min="1" required />
+          </div>
+          <div>
+            <label>Children</label>
+            <input type="number" id="child" name="total_children" placeholder="2" min="0" required />
+          </div>
+          <div>
+            <label>Check-in Date</label>
+            <input type="date" id="checkin-date" name="checkin" required />
+          </div>
+          <div>
+            <label>Check-out Date</label>
+            <input type="date" id="checkout-date" name="checkout" required />
+          </div>
+          <div>
+            <label >Select Room Preference</label>
+            <select id="room-selection" name="room_preference" required>
+              <option value="">Choose a Room from the List</option>
+              <option value="connecting">Connecting</option>
+              <option value="adjoining">Adjoining</option>
+              <option value="adjacent">Adjacent</option>
+            </select>
+          </div>
+          <hr />
+          <div>
+            <label>Anything Else?</label>
+            <textarea id="message" name="visitor_message" placeholder="Tell us anything else that might be important." required></textarea>
+          </div>
+          <button type="submit">Book The Rooms</button>
+        </form> */}
         <Information />
         <DesignBlocks />
         <Pages />
