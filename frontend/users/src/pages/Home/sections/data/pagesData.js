147e8dc93f5079a23527b8edb8598958a0988ac7
vidz0509 <vairagbavadiya96@gmail.com>
2024-01-04 22:55:48 +0530

    color change

--- frontend/users/src/pages/Home/sections/data/pagesData.js
+++ frontend/users/src/pages/Home/sections/data/pagesData.js
@@ -20,22 +20,22 @@ export default [
   {
     image: `${imagesPrefix}/about-us.jpg`,
     name: "About Us Page",
-    route: "/pages/landing-pages/about-us",
+    route: "/about-us",
   },
   {
     image: `${imagesPrefix}/contact.jpg`,
     name: "Contact Us Page",
-    route: "/pages/landing-pages/contact-us",
+    route: "/contact-us",
   },
   {
     image:
       "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/material-design-system/presentation/account/signin-basic.jpg",
     name: " Sign In Page",
-    route: "/pages/authentication/sign-in",
+    route: "/sign-in",
   },
   {
     image: `${imagesPrefix}/author.jpg`,
     name: "Author Page",
-    route: "/pages/landing-pages/author",
+    route: "/author",
   },
 ];
