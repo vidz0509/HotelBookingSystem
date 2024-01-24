cbdfce99515374121fd56a86fd823f817c8f01bd
darshu <darshanmangukiya4@gmail.com>
2024-01-24 23:30:37 +0530

    hotel

---
+++ frontend/users/src/pages/LandingPages/Hotels/index.js
@@ -0,0 +1,96 @@
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+import Information from "pages/Home/sections/Information";
+import DesignBlocks from "pages/Home/sections/DesignBlocks";
+import Pages from "pages/Home/sections/Pages";
+import Testimonials from "pages/Home/sections/Testimonials";
+import Download from "pages/Home/sections/Download";
+import SearchForm from "pages/Home/sections/SearchForm";
+import routes from "routes";
+
+import footerRoutes from "footer.routes";
+import bgImage from "assets/images/vestrahorn.webp";
+
+function Hotels() {
+  return (
+    <>
+      <MKBox position="fixed" top="0.5rem" width="100%">
+        <DefaultNavbar
+          routes={routes}
+          sticky
+        />
+      </MKBox>
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="white"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Luxury Hotels{" "}
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="white"
+              textAlign="center"
+              px={{ xs: 6, lg: 12 }}
+              mt={1}
+            >
+              The Best Place In Town To Relax, To Enjoy Heaven
+            </MKTypography>
+          </Grid>
+          <Grid container item xs={12} lg={10} justifyContent="center" mx="auto" className="form-wrap">
+            <SearchForm />
+          </Grid>
+        </Container>
+      </MKBox>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+          backdropFilter: "saturate(200%) blur(30px)",
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        
+        <Information />
+        <DesignBlocks />
+        <Pages />
+        
+        <Testimonials />
+        <Download />
+      </Card>
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default Hotels;
