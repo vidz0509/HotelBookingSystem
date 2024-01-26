a9e8e069db6e5f964182a39358c7505380b254e1
darshu <darshanmangukiya4@gmail.com>
2024-01-26 21:21:18 +0530

    footer adminside

--- frontend/admin/src/components/footer/Footer.jsx
+++ frontend/admin/src/components/footer/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row">
       <h5 className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
-        <p className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
-          ©{1900 + new Date().getYear()} . All Rights Reserved.
-        </p>
+        {/* <p className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
+          ©{1900 + new Date().getYear()} ..
+        </p> */}
       </h5>
       <div>
         <ul className="flex flex-wrap items-center gap-3 sm:flex-nowrap md:gap-10">
