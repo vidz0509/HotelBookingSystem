ea716df243e15c4ef0c6b02bbb8527f3578b2ae9
Yashbhimani623 <bhimaniyash623@gmail.com>
2024-01-31 22:53:12 +0530

    Dashboard Count Design

--- frontend/admin/src/views/admin/dashboard/index.jsx
+++ frontend/admin/src/views/admin/dashboard/index.jsx
@@ -2,11 +2,11 @@
 import WeeklyRevenue from "views/admin/dashboard/components/WeeklyRevenue";
 import TotalSpent from "views/admin/dashboard/components/TotalSpent";
 // import PieChartCard from "views/admin/dashboard/components/PieChartCard";
-import { IoMdHome } from "react-icons/io";
-import { IoDocuments } from "react-icons/io5";
-import { MdBarChart, MdDashboard } from "react-icons/md";
+// import { IoMdHome } from "react-icons/io";
+// import { IoDocuments } from "react-icons/io5";
+// import { MdBarChart, MdDashboard } from "react-icons/md";
 
-import {  columnsDataComplex } from "./variables/columnsData";
+import { columnsDataComplex } from "./variables/columnsData";
 // columnsDataCheck,
 import Widget from "components/widget/Widget";
 // import CheckTable from "views/admin/dashboard/components/CheckTable";
@@ -16,42 +16,51 @@ import ComplexTable from "views/admin/dashboard/components/ComplexTable";
 // import tableDataCheck from "./variables/tableDataCheck.json";
 import tableDataComplex from "./variables/tableDataComplex.json";
 
+import {
+  MdSupervisedUserCircle,
+  MdMenuBook,
+  MdPublic,
+  MdPayments,
+  MdHotel,
+  MdMyLocation,
+} from "react-icons/md";
+
 const Dashboard = () => {
   return (
     <div>
       {/* Card widget */}
-      
+
 
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"Earnings"}
-          subtitle={"$340.5"}
+          icon={<MdSupervisedUserCircle className="h-7 w-7" />}
+          title={"Customers"}
+          subtitle={""}
         />
         <Widget
-          icon={<IoDocuments className="h-6 w-6" />}
-          title={"Spend this month"}
-          subtitle={"$642.39"}
+          icon={<MdPublic className="h-6 w-6" />}
+          title={"Countries"}
+          subtitle={""}
         />
         <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"Sales"}
-          subtitle={"$574.34"}
+          icon={<MdMyLocation className="h-7 w-7" />}
+          title={"Locations"}
+          subtitle={""}
         />
         <Widget
-          icon={<MdDashboard className="h-6 w-6" />}
-          title={"Your Balance"}
-          subtitle={"$1,000"}
+          icon={<MdHotel className="h-6 w-6" />}
+          title={"Hotels"}
+          subtitle={""}
         />
         <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"New Tasks"}
-          subtitle={"145"}
+          icon={<MdMenuBook className="h-7 w-7" />}
+          title={"Bookings"}
+          subtitle={""}
         />
         <Widget
-          icon={<IoMdHome className="h-6 w-6" />}
-          title={"Total Projects"}
-          subtitle={"$2433"}
+          icon={<MdPayments className="h-6 w-6" />}
+          title={"Payment Status"}
+          subtitle={""}
         />
       </div>
 
