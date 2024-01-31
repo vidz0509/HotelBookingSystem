ce19d0b531c22a19194964725a01a0a1c6d54d50
vidz0509 <vairagbavadiya96@gmail.com>
2024-01-31 22:11:22 +0530

    dashboard navbar header

--- frontend/admin/src/components/navbar/index.jsx
+++ frontend/admin/src/components/navbar/index.jsx
@@ -38,7 +38,7 @@ const Navbar = (props) => {
 
 
   return (
-    <nav className="sticky top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
+    <nav className="sticky top-0 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
       <div className="ml-[6px]">
         {
           brandText !== "Dashboard" &&
