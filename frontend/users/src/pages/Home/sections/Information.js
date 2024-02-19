08dfb52813f613e8f37453bafe009c19063a2cea
darshu <darshanmangukiya4@gmail.com>
2024-02-19 12:19:00 +0530

    amenities

--- frontend/users/src/pages/Home/sections/Information.js
+++ frontend/users/src/pages/Home/sections/Information.js
@@ -20,61 +20,30 @@ import Grid from "@mui/material/Grid";
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 
-// Material Kit 2 React examples
-import RotatingCard from "examples/Cards/RotatingCard";
-import RotatingCardFront from "examples/Cards/RotatingCard/RotatingCardFront";
-import RotatingCardBack from "examples/Cards/RotatingCard/RotatingCardBack";
+  
 import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
 
-// Images
-import bgFront from "assets/images/rotating-card-bg-front.jpeg";
-import bgBack from "assets/images/rotating-card-bg-back.jpeg";
+
 
 function Information() {
   return (
     <MKBox component="section" py={3} my={2}>
       <Container>
         <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
-          <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
-            <RotatingCard>
-              <RotatingCardFront
-                image={bgFront}
-                icon="touch_app"
-                title={
-                  <>
-                    Feel the
-                    <br />
-                    Material Kit
-                  </>
-                }
-                description="All the MUI components that you need in a development have been re-design with the new look."
-              />
-              <RotatingCardBack
-                image={bgBack}
-                title="Discover More"
-                description="You will save a lot of time going from prototyping to full-functional code because all elements are implemented."
-                action={{
-                  type: "internal",
-                  route: "/sections/page-sections/page-headers",
-                  label: "start with header",
-                }}
-              />
-            </RotatingCard>
-          </Grid>
           <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
             <Grid container spacing={3}>
               <Grid item xs={12} md={6}>
                 <DefaultInfoCard
                   icon="sports_gymnastics"
                   title="Gym"
-                  description="A gym is a building, or large room, usually containing special equipment, where people go to do physical exercise and get fit."
+                
                 />
               </Grid>
               <Grid item xs={12} md={6}>
                 <DefaultInfoCard
                   icon="sports_tennis"
                   title="Playground"
-                  description="A playground is an area used by children to play in. They usually have equipment such as slides, swings and seesaws for children to play on."
+                
                 />
               </Grid>
             </Grid>
@@ -83,14 +52,13 @@ function Information() {
                 <DefaultInfoCard
                   icon="pool"
                   title="Swimming Pool"
-                  description="It will have a natural uneven shape, and will often have features around it like rocks and waterfalls to compliment the naturalistic style."
+                  
                 />
               </Grid>
               <Grid item xs={12} md={6}>
                 <DefaultInfoCard
                   icon="local_bar"
                   title="Club"
-                  description="The club is electric tonight, everyone feeding off of the smiles and fast dancing."
                 />
               </Grid>
             </Grid>
