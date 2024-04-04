351da598e364a444b4fb28f90b6cb9c8e7ec614a
Yashbhimani623 <bhimaniyash623@gmail.com>
2024-04-04 12:29:11 +0530

    comment remove

--- frontend/admin/src/views/admin/amenities/add.jsx
+++ frontend/admin/src/views/admin/amenities/add.jsx
@@ -76,7 +76,6 @@ export default function AddAmenities() {
   return (
     <form>
       <div className=" flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-start">
-        {/* Sign in section */}
         <div className="mt-[1vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
           <InputField
             variant="auth"
--- frontend/admin/src/views/admin/hotels/edit.jsx
+++ frontend/admin/src/views/admin/hotels/edit.jsx
@@ -247,7 +247,6 @@ export default function EditHotel() {
   return (
     <form>
       <div className=" flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-start">
-        {/* Sign in section */}
         <div className="mt-[1vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0">
           <div className="hotel-data-fields xl:max-w-[420px]">
             <InputField
